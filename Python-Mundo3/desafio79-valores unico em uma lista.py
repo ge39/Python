4dd1850f2c7a9122ad2f825124fc9221f52e7bef
Jeremias <ge39.medeiros@gmail.com>
2020-02-22 19:21:36 -0300

    desafio79-valores unico em uma lista

--- Python-Mundo3/desafio79-valores unico em uma lista.py
+++ Python-Mundo3/desafio79-valores unico em uma lista.py
@@ -5,8 +5,26 @@
     No final, serão exibidos todos os valores unicos digitados em ordem crescente
 '''
 valor = []
-
+dupl = []
+resp = 'S'
 
 while True:
-    num = valor.append(int(input('Digite um numero :')))
-    print(num)
+    num = int(input('Digite um numero :'))
+    resp = str(input('Quer continuar: [S/N]')).strip().upper()
+
+    if num not in valor:
+        valor.append(num)
+    else:
+        dupl.append(num)
+        print(f'Valor\033[31m {num}\033[m já foi inserido')
+    if resp in 'Ss':
+        continue
+    else:
+        break
+valor.sort()
+print(f'\033[33mValores inseridos na lista foram\033[m -> {valor}')
+print(f'Valores Duplicados\033[31m {dupl}\033[m ')
+
+
+
+
