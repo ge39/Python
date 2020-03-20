1b2b6974c20705fe305700361807df2365803c5f
Jeremias <ge39.medeiros@gmail.com>
2020-03-20 16:25:40 -0300

    desafio80-lista ordenada sem repeticao

--- Python-Mundo3/desafio80-lista ordenada sem repeticao.py
+++ Python-Mundo3/desafio80-lista ordenada sem repeticao.py
@@ -4,9 +4,24 @@
     No final, mostre a lista ordenada na tela.
 
 '''
+lista = []
+for c in range(0, 5):
+    n = int(input("Digite um numero"))
 
-valor = []
-for c in range(1, 6):
-   num = int(input(f'Digite o {c}º numero:'))
-   valor.append(num)
-print(f'Lista ordenada {valor}')
+    if c == 0 or n > lista[-1]:
+       # se for o primeiro numero digitado ou se  for maior que o numero na lista, insere no final
+       lista.append(n)
+       print('Adicionado ao final da lista')
+    else:
+       #se nao for o primeiro numero e nem o ultimo, o numero será inserido no meio
+      pos = 0  # posiçao zero do array
+      while pos < len(lista):
+         #se o numero digitado for menor ou igual que o numero da lista , determine a posição a inserir na lista
+         if n <= lista[pos]:
+            print(lista[0])
+            lista.insert(pos, n)
+            print(f'Adicionado na posição {pos} da lista...')
+            break
+         pos +=1
+print(f'Os valores digitados em ordem foram {lista}')
+
